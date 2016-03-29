d60b5502a1e7584d6a29a4d02557662dea1dc733
Nicholas Reichert <joe13reich@gmail.com>
2016-03-28 21:10:29 -0400

    Updated ProgrammerAssertions to support Swift 2.2.

--- Assertions/ProgrammerAssertions.swift
+++ Assertions/ProgrammerAssertions.swift
@@ -21,37 +21,37 @@ import Foundation
 
 /// drop-in replacements
 
-public func assert(@autoclosure condition: () -> Bool, @autoclosure _ message: () -> String = "", file: StaticString = __FILE__, line: UInt = __LINE__) {
+public func assert(@autoclosure condition: () -> Bool, @autoclosure _ message: () -> String = "", file: StaticString = #file, line: UInt = #line) {
     Assertions.assertClosure(condition(), message(), file, line)
 }
 
-public func assertionFailure(@autoclosure message: () -> String = "", file: StaticString = __FILE__, line: UInt = __LINE__) {
+public func assertionFailure(@autoclosure message: () -> String = "", file: StaticString = #file, line: UInt = #line) {
     Assertions.assertionFailureClosure(message(), file, line)
 }
 
-public func precondition(@autoclosure condition: () -> Bool, @autoclosure _ message: () -> String = "", file: StaticString = __FILE__, line: UInt = __LINE__) {
+public func precondition(@autoclosure condition: () -> Bool, @autoclosure _ message: () -> String = "", file: StaticString = #file, line: UInt = #line) {
     Assertions.preconditionClosure(condition(), message(), file, line)
 }
 
-@noreturn public func preconditionFailure(@autoclosure message: () -> String = "", file: StaticString = __FILE__, line: UInt = __LINE__) {
+@noreturn public func preconditionFailure(@autoclosure message: () -> String = "", file: StaticString = #file, line: UInt = #line) {
     Assertions.preconditionFailureClosure(message(), file, line)
     runForever()
 }
 
-@noreturn public func fatalError(@autoclosure message: () -> String = "", file: StaticString = __FILE__, line: UInt = __LINE__) {
+@noreturn public func fatalError(@autoclosure message: () -> String = "", file: StaticString = #file, line: UInt = #line) {
     Assertions.fatalErrorClosure(message(), file, line)
     runForever()
 }
 
 /// Stores custom assertions closures, by default it points to Swift functions. But test target can override them.
 public class Assertions {
-
+    
     public static var assertClosure              = swiftAssertClosure
     public static var assertionFailureClosure    = swiftAssertionFailureClosure
     public static var preconditionClosure        = swiftPreconditionClosure
     public static var preconditionFailureClosure = swiftPreconditionFailureClosure
     public static var fatalErrorClosure          = swiftFatalErrorClosure
-
+    
     public static let swiftAssertClosure              = { Swift.assert($0, $1, file: $2, line: $3) }
     public static let swiftAssertionFailureClosure    = { Swift.assertionFailure($0, file: $1, line: $2) }
     public static let swiftPreconditionClosure        = { Swift.precondition($0, $1, file: $2, line: $3) }
